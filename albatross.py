514cb5e5ed0be19c5cc9aeae54e77a431696993b
Chales Horn <charles.horn@gmail.com>
2022-11-02 17:51:23 +1300

    tmp output for reading exact page content tests for various page numbers will become part of the story output

--- albatross.py
+++ albatross.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+import subprocess
 import sys
 
 from albatross.reader import Reader
@@ -9,11 +11,13 @@ from albatross.plot import J1, J2
 TITLE = "Perspective of an Albatross"
 NL = '\n'
 
+PNUM = re.compile(r'\d+$|CHAPTER 1$')
 
 class Book():
     def __init__(self, title):
         self.title = title
         self.chapters = {}
+        self.pdf = 'output.pdf'
 
     def append(self, chapter, content):
         """Append content to chapter"""
@@ -22,21 +26,75 @@ class Book():
         else:
             self.chapters[chapter] = [content]
 
+    def get_page(self, n):
+        """Returns the text of page n."""
+        # TODO: memoise pages
+        # Save current book state to pdf and convert to txt to determine current page breaks...
+        self.to_pdf()
+        subprocess.run(['pdftotext', self.pdf, 'pdf.txt'], capture_output=True)
+        pages = []
+        pb = ''
+        with open('pdf.txt', 'r') as f:
+            for line in f:
+                line = line.lstrip()
+                if not line:
+                    continue
+                if PNUM.match(line):  # page number found
+                    pages.append(pb)
+                    pb = ''
+                else:
+                    pb += line
+        p = pages[n]
+        return p
+
     def show(self):
         """Ouput Book to STDOUT."""
-        print('#', self.title, '\n')
-        print('\\pagenumbering{gobble}')
+        print(self.to_markdown())
+
+    def to_markdown(self):
+        output = [f'# {self.title}',
+            '\\pagenumbering{gobble}']
         for k, c in self.chapters.items():
-            print('\\newpage')
-            print(f'\n## CHAPTER {k}\n\n')
+            output.append('\\newpage')
+            output.append(f'\n## CHAPTER {k}\n\n')
             if k == 1:
-                print('\\newpage')
-                print('\\pagenumbering{arabic}')
-            print('\n'.join(c))
+                output += ['\\newpage', '\\pagenumbering{arabic}']
+            output += c
+        return NL.join(output)
+
+    def to_pdf(self):
+         md = self.to_markdown()
+         with open('output.md', 'w') as f:
+             f.write(md)
+         subprocess.run(['pandoc', 'output.md', '-o', self.pdf], capture_output=True)
 
 
 def get_sentences(text, n):
+    # TODO: refactor!!!
     return NL.join(text.split(NL)[:n])
+
+
+def test_book_get(book):
+    # TODO: refactor!!!
+    TA = 6
+    TB = 9
+
+    a = f"The first letter on page 1 of this book is '{book.get_page(1)[0]}'."
+    n = 2
+    c = book.get_page(2)[0]
+    b = f"The first letter on page {n} of this book is '{c}'."
+    d = book.get_page(7).split(NL)[3].replace(' ', '')[5]
+    tc3 = f"The fifth letter of the third line on page 7 is '{d}'."
+    book.append(TB, a)
+    book.append(TB, b)
+    book.append(TB, tc3)
+    book.append(TA, f'''From flicking through the pages earlier, the reader had noticed that chapter {TB} starts with a number of statements about specific letters located throughout the book.
+The first was that "{a}".
+This appears to be correct.
+Then "{b}".
+Glancing at the facing page, this too is verified as correct.
+"{tc3}; flicking forward {7-1} pages, the reader finds that the fifth letter of the third line is indeed '{d}'.
+    ''')
 
 
 def story(seedfile):
@@ -60,6 +118,8 @@ def story(seedfile):
 
     book.append(3, c)
     book.append(1, J2)
+
+    test_book_get(book)
 
     return book
 
